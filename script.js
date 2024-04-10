865b062738fc656817fbe6968ffd999822348297
deeprathod1 <deeprathod788@gmail.com>
2024-04-10 11:46:10 +0530

    javascript-portfolio

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById("deep").innerHTML = `<h1>Deep Rathod  </h1>
+<img src="https://media-bom2-2.cdn.whatsapp.net/v/t61.24694-24/425375048_1057399472005316_4092018716004373423_n.jpg?ccb=11-4&oh=01_ASDgBwbkCFadbvHfSv7zwdwoiZlWpo5laOVdAV2-GLQOGg&oe=66231B43&_nc_sid=e6ed6c&_nc_cat=110" alt="" >
+<h2>Fullstack devlopment </h2>
+<p>Welcome To My Protfolio I Am Deep Rathod </p>
+<h3>Skills</h3>
+<ul>
+    <li id ="py">Python</li>
+    <li id = "ht">HTML</li>
+    <li id = "css">CSS</li>
+    <li id = "jq">JQuery</li>
+    <li id = "bt">Bootsrap</li>
+</ul>
+<ol>
+    <li>
+        <h4>Protfolio Website</h4>
+    </li>
+    <p>A personal portfolio website showcasing my skills and projects.</p>
+    <a target="_blank" href="https://65fc2a9be782310d582f40ea--sparkly-heliotrope-e82bf4.netlify.app/#hero">link</a>
+    <li>
+        <h4> Deep.com</h4>
+    </li>
+    <p>coding websit </p>
+    <a target="_blank" href="https://65be6ec9d164b6b03fbeb071--marvelous-lily-d0f9de.netlify.app/">link</a>
+</ol>
+<h2> Contact</h2>
+<p>You can reach me at:<a href="mailto:deeprathod788@gmail.com">Gmail</a></p>
+<p>© 2023 Deep Rathod. All rights reserved.</p>`;
